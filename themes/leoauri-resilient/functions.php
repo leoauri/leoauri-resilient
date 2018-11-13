9fc17ad310d0b3a2fa2ca7e32f2565f7c1808b2b
Leo Auri <code@leoauri.com>
2018-11-13 12:39:36 +0100

    Remove native emoji bloat

--- themes/leoauri-resilient/functions.php
+++ themes/leoauri-resilient/functions.php
@@ -62,3 +62,13 @@ function register_shorts_shortcode() {
   add_shortcode('show_shorts', '\leoauriResilient\show_shorts');
 }
 add_action('init', '\leoauriResilient\register_shorts_shortcode');
+
+
+// purge native emoji integration
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+remove_action('wp_head', 'wp_resource_hints', 2);
