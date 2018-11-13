36a7a080f012de5fc2061589e67ad2bdf92b2f05
Leo Auri <code@leoauri.com>
2018-11-13 12:49:45 +0100

    Correct license link and html tag classes

--- themes/leoauri-resilient/header.php
+++ themes/leoauri-resilient/header.php
@@ -7,7 +7,7 @@ namespace leoauriResilient;
 
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js no-svg">
+<html <?php language_attributes(); ?>>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -16,7 +16,7 @@ namespace leoauriResilient;
 <meta name="description" content="Homepage Leo Auri, musician and composer" />
 <link rel="preload" href="<?php echo get_template_directory_uri(); ?>/js/navbar.js" as="script" />
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css" />
-<link rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/" />
+<link rel="license" href="https://github.com/leoauri/leoauri-resilient/blob/master/LICENSE" />
 
 <?php wp_head(); ?>
 </head>
