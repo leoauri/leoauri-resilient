37e19de3f12d785285fb4b3d150fd0848e6119d5
Leo Auri <code@leoauri.com>
2018-11-19 14:52:50 +0100

    Track admin

---
+++ themes/leoauri-resilient/admin/parts/track_meta_boxes.php
@@ -0,0 +1,43 @@
+<?php
+namespace leoauriResilient;
+/**
+ * leoauri Resilient
+ **/
+
+
+// <iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/album=3440820255/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=2991266595/transparent=true/" seamless><a href="http://leoauri.bandcamp.com/album/the-call">The Call by Leo Auri</a></iframe>
+?>
+<div>
+  <label>
+    <span>Track ID</span>
+    <input type="text" name="track-ID" value="<?php echo get_post_meta($object->ID, "track-ID", true); ?>">
+  </label>
+</div>
+<div>
+  <label>
+    <span>Album ID</span>
+    <input type="text" name="album-ID" value="<?php echo get_post_meta($object->ID, "album-ID", true); ?>">
+  </label>
+</div>
+<div>
+  <label>
+    <span>Fallback link</span>
+    <input type="text" name="fallback-link" value="<?php echo get_post_meta($object->ID, "fallback-link", true); ?>">
+  </label>
+</div>
+<div>
+  <label>
+    <span>Fallback text</span>
+    <input type="text" name="fallback-text" value="<?php echo get_post_meta($object->ID, "fallback-text", true); ?>">
+  </label>
+</div>
+<div>
+  <label>
+    <span>From Album Post</span>
+    <select name="from-album-post">
+      <?php foreach ($albumposts as $post) : ?>
+      <option value="<?php echo $post->ID; ?>"<?php if ($post->ID == get_post_meta($object->ID, "from-album-post", true)) { echo " selected"; } ?>><?php echo $post->post_title; ?></option>
+      <?php endforeach; ?>
+    </select>
+  </label>
+</div>
--- themes/leoauri-resilient/functions.php
+++ themes/leoauri-resilient/functions.php
@@ -130,3 +130,134 @@ function enqueue_scripts() {
   wp_deregister_script('wp-embed');
 }
 add_action('wp_enqueue_scripts', '\leoauriResilient\enqueue_scripts');
+
+
+// Remove admin Links section
+function customise_admin_menu() {
+ remove_menu_page('link-manager.php');
+}
+add_action('admin_menu', '\leoauriResilient\customise_admin_menu');
+
+
+// Add track post type
+function add_track_post_type() {
+  $labels = array(
+    'name'                  => 'Tracks',
+    'singular_name'         => 'Track',
+    'menu_name'             => 'Tracks',
+    'name_admin_bar'        => 'Track',
+    'archives'              => 'Track Archives',
+    'attributes'            => 'Track Attributes',
+    'parent_item_colon'     => 'Parent Item:',
+    'all_items'             => 'All Tracks',
+    'add_new_item'          => 'Add New Track',
+    'add_new'               => 'Add New',
+    'new_item'              => 'New Track',
+    'edit_item'             => 'Edit Track',
+    'update_item'           => 'Update Track',
+    'view_item'             => 'View Track',
+    'view_items'            => 'View Tracks',
+    'search_items'          => 'Search Track',
+    'not_found'             => 'Not found',
+    'not_found_in_trash'    => 'Not found in Trash',
+    'featured_image'        => 'Featured Image',
+    'set_featured_image'    => 'Set featured image',
+    'remove_featured_image' => 'Remove featured image',
+    'use_featured_image'    => 'Use as featured image',
+    'insert_into_item'      => 'Insert into Track',
+    'uploaded_to_this_item' => 'Uploaded to this Track',
+    'items_list'            => 'Tracks list',
+    'items_list_navigation' => 'Tracks list navigation',
+    'filter_items_list'     => 'Filter Track list',
+  );
+  $args = array(
+    'label'                 => 'Track',
+    'description'           => 'Individual Bandcamp Tracks',
+    'labels'                => $labels,
+    'supports'              => array('title', 'page-attributes'),
+    'taxonomies'            => array('category', 'post_tag'),
+    'hierarchical'          => false,
+    'public'                => true,
+    'show_ui'               => true,
+    'show_in_menu'          => true,
+    'menu_position'         => 20,
+    'menu_icon'             => 'dashicons-media-audio',
+    'show_in_admin_bar'     => true,
+    'show_in_nav_menus'     => true,
+    'can_export'            => true,
+    'has_archive'           => false,
+    'exclude_from_search'   => false,
+    'publicly_queryable'    => false,
+    'capability_type'       => 'page',
+  );
+  register_post_type('lr_track', $args);
+}
+add_action('init', '\leoauriResilient\add_track_post_type', 0);
+
+function track_meta_markup($object) {
+  wp_nonce_field(basename(__FILE__), 'track-meta-nonce');
+
+  $albumquery = new \WP_Query(array(
+    'category_name' => 'albums',
+    'posts_per_page' => -1,
+  ));
+  $albumposts = $albumquery->posts;
+
+  ob_start();
+  require(get_stylesheet_directory() . '/admin/parts/track_meta_boxes.php');
+  echo ob_get_clean();
+
+}
+
+function track_meta_boxes() {
+  add_meta_box('lr_track_settings', 'Track Settings', '\leoauriResilient\track_meta_markup', 'lr_track', 'normal', 'high', null);
+}
+add_action('add_meta_boxes_lr_track', '\leoauriResilient\track_meta_boxes');
+
+
+function save_track_meta_boxes($post_id, $post) {
+  // Check nonce
+  if (! isset($_POST['track-meta-nonce']) || ! wp_verify_nonce($_POST['track-meta-nonce'], basename(__FILE__))) {
+    return $post_id;
+  }
+  // Check if the current user has permission to edit the post.
+  if (! current_user_can('edit_post', $post_id)) {
+    return $post_id;
+  }
+  if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+    return $post_id;
+  }
+
+  $metafields = array(
+    "track-ID",
+    "album-ID",
+    "fallback-link",
+    "fallback-text",
+    "from-album-post",
+  );
+
+  foreach ($metafields as $metafield) {
+    $new_meta_value = $_POST[$metafield];
+    $current_meta_value = get_post_meta($post_id, $metafield, true);
+
+    /* If a new meta value was added and there was no previous value, add it. */
+    if ($new_meta_value && ! $current_meta_value) {
+      add_post_meta($post_id, $metafield, $new_meta_value, true);
+    }
+    /* If there is no new meta value but an old value exists, delete it. */
+    elseif (! $new_meta_value && $current_meta_value) {
+      delete_post_meta($post_id, $metafield, $current_meta_value);
+    }
+    /* If the new meta value does not match the old value, update it. */
+    elseif ($new_meta_value != $current_meta_value) {
+      update_post_meta($post_id, $metafield, $new_meta_value, $current_meta_value);
+    }
+  }
+}
+add_action('save_post_lr_track', '\leoauriResilient\save_track_meta_boxes', 10, 2);
+
+function admin_scripts() {
+  wp_register_style('lr_track_admin_style', get_template_directory_uri() . '/admin/css/track.css', false, '0.1.1');
+  wp_enqueue_style('lr_track_admin_style');
+}
+add_action('admin_enqueue_scripts', '\leoauriResilient\admin_scripts');
