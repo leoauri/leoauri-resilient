e03ef31670e2db984a0af2a47f018748e66257df
Leo Auri <code@leoauri.com>
2019-01-31 23:47:34 +0100

    Don't show related YouTube videos

--- plugins/vimeo-embed/parts/embed.php
+++ plugins/vimeo-embed/parts/embed.php
@@ -11,7 +11,10 @@ namespace leoauriVimeoEmbed;
 <iframe src='https://player.vimeo.com/video/<?php echo $id; ?>' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen>
 <?php break; ?>
 <?php case 'youtube': ?>
-<iframe src='https://www.youtube-nocookie.com/embed/<?php echo $id; ?>' frameborder='0' allowfullscreen>
+<iframe src='https://www.youtube-nocookie.com/embed/<?php echo $id; ?>?rel=0' frameborder='0' allowfullscreen>
+<?php break; ?>
+<?php case 'youtube_playlist': ?>
+<iframe src='https://www.youtube-nocookie.com/embed/videoseries?list=<?php echo $id; ?>' frameborder='0' allowfullscreen>
 <?php break; ?>
 <?php endswitch; ?>
 </iframe>
